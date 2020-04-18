3418925bcb60085e9cd77e64f07c534c343366ef
Nate Olsen <nso4wg@virginia.edu>
2020-04-18 02:34:44 -0400

    added dates/converted it to csv for easier conversion to pandas

--- gettweets.py
+++ gettweets.py
@@ -1,7 +1,10 @@
+# NO NEED TO RERUN THIS FILE
+
 import tweepy
 from dotenv import load_dotenv
 from os.path import join, dirname
 import os
+import pandas as pd
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -16,28 +19,35 @@ auth.set_access_token(access_token_key, access_token_secret)
 
 api = tweepy.API(auth)
 
-#print(api.VerifyCredentials())
 t1 = api.user_timeline(screen_name="realDonaldTrump", count=200, tweet_mode='extended')
 t2 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1248475288171761670, tweet_mode='extended')
 t3 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1245754978880241664, tweet_mode='extended')
 t4 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1242414528031227902, tweet_mode='extended')
 
 t = t1+t2+t3[1:]+t4[1:]
-len(t)
+
 tweets = []
+created_at = []
 for tweet in t:
     if 'https://t.co' in tweet.full_text:
         continue
     if tweet.full_text[0:2] == 'RT':
-        tweets.append(tweet.retweeted_status.full_text)
+        tweets.append(tweet.retweeted_status.full_text.replace('\n', ' '))
     else:
-        tweets.append(tweet.full_text)
+        tweets.append(tweet.full_text.replace('\n', ' '))
+    created_at.append(tweet.created_at)
 
+df = pd.DataFrame({"text":tweets, "date":created_at})
+df.date = df.date.dt.tz_localize('UTC').dt.tz_convert('US/Eastern')
+df.to_csv('tweets.csv')
+
+"""
 file = open("tweets.txt", "w")
 
-for i in tweets:
-    i = i.replace('\n', ' ')
-    file.write(i.lower())
+for i, j in zip(tweets, created_at):
+    text = i.replace('\n', ' ')
+    file.write(text.lower() + ' :: ' + str(j))
     file.write('\n')
 
 file.close()
+"""
