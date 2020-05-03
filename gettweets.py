eb84a1cdea7816366488664ce48909fefac10295
Nate Olsen <nso4wg@virginia.edu>
2020-05-03 01:24:16 -0400

    made improvements, added more stock data, and implemented word2vec

--- gettweets.py
+++ gettweets.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from os.path import join, dirname
 import os
 import pandas as pd
+import re
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -19,35 +20,33 @@ auth.set_access_token(access_token_key, access_token_secret)
 
 api = tweepy.API(auth)
 
-t1 = api.user_timeline(screen_name="realDonaldTrump", count=200, tweet_mode='extended')
+t1 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1252042796151017472, tweet_mode='extended')
 t2 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1249042796151017472, tweet_mode='extended')
 t3 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1246459270352207879, tweet_mode='extended')
 t4 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1243312056805318656, tweet_mode='extended')
+t5 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1241570447344447488, tweet_mode='extended')
+t6 = api.user_timeline(screen_name="realDonaldTrump", count=200, max_id = 1239201512171192321, tweet_mode='extended')
 
-t = t1+t2[1:]+t3[1:]+t4[1:]
+#print(t5[198].id)
+
+t = t1+t2[1:]+t3[1:]+t4[1:]+t5[1:]+t6[1:]
 
 tweets = []
 created_at = []
 for tweet in t:
-    if 'https://t.co' in tweet.full_text:
-        continue
     if tweet.full_text[0:2] == 'RT':
-        tweets.append(tweet.retweeted_status.full_text.replace('\n', ' ').lower())
+        no_links = re.sub('(https://).*', '', tweet.retweeted_status.full_text)
+        rmv_spec = re.sub('[^A-Za-z0-9 ]+', '', no_links)
+        if rmv_spec != '':
+            tweets.append(rmv_spec.replace('\n', ' ').lower())
     else:
-        tweets.append(tweet.full_text.replace('\n', ' ').lower())
-    created_at.append(tweet.created_at)
+        no_links = re.sub('(https://).*', '', tweet.full_text)
+        rmv_spec = re.sub('[^A-Za-z0-9 ]+', '', no_links)
+        if rmv_spec != '':
+            tweets.append(rmv_spec.replace('\n', ' ').lower())
+    if rmv_spec != '':
+        created_at.append(tweet.created_at)
 
 df = pd.DataFrame({"text":tweets, "date":created_at})
 df.date = df.date.dt.tz_localize('UTC').dt.tz_convert('US/Eastern')
 df.to_csv('tweets.csv', index=False)
-
-"""
-file = open("tweets.txt", "w")
-
-for i, j in zip(tweets, created_at):
-    text = i.replace('\n', ' ')
-    file.write(text.lower() + ' :: ' + str(j))
-    file.write('\n')
-
-file.close()
-"""
--- project.py
+++ project.py
@@ -2,8 +2,10 @@
 
 import pandas as pd
 import datetime
+import word2vec
+
 tweets = pd.read_csv('tweets.csv', parse_dates= ['date'])
-sp500 = pd.read_csv('^GSPC.csv', parse_dates= ['Date'])
+sp500 = pd.read_csv('CL=F.csv', parse_dates= ['Date'])
 
 
 
@@ -20,13 +22,15 @@ Train on 70%, Test on 30%
 
 sp500['Diff'] = sp500['Adj Close'] - sp500['Open']
 
-for i in sp500['Diff']:
-    print(i)
+#for i in sp500['Diff']:
+#    print(i)
 
 tweets['date'] = tweets['date'].dt.date
 sp500['Date'] = sp500['Date'].dt.date
 
-tweets['stock_mov'] = 537 * [0]
+#len(tweets['date'])
+
+tweets['stock_mov'] = 1122 * [0]
 
 # Creates a column in tweets dataframe that assigns the stock movement to the previous days tweets (days where the sotck market was closed are 0)
 diff1 = datetime.timedelta(days=1)
@@ -39,25 +43,39 @@ for i in range(len(sp500['Date'])):
 # might want to group all @s and videos together!
 words_weight = {}
 # 70% is up to 376
-for i in tweets['text'][0:400]:
+for i in tweets['text'][0:900]:
 #    print(i)
-#    print(tweets.loc[tweets['text'] == i, 'stock_mov'].iloc[0])
+
+    #print(tweets.loc[tweets['text'] == i, 'stock_mov'].iloc[0])
     for j in i.split():
         if j not in words_weight:
             words_weight[j] = 0.01 * tweets.loc[tweets['text'] == i, 'stock_mov'].iloc[0]
         else:
             words_weight[j] += 0.01*tweets.loc[tweets['text'] == i, 'stock_mov'].iloc[0]
 
+
+model = word2vec.load('text8.bin')
 # TESTING
 correct = 0
 wrong = 0
-for i in tweets['text'][400:]:
+for i in tweets['text'][900:]:
     guess = 0
     for j in i.split():
         if j in words_weight:
             guess += words_weight[j]
         else:
-            guess += 0
+            try:
+                indexes, metrics = model.similar(j)
+                similar_words = model.generate_response(indexes, metrics).tolist()
+                for k in similar_words:
+                    if k[0] in words_weight:
+                        # print(words_weight[k[0]])
+                        guess += words_weight[k[0]]
+                        break
+                guess += 0
+            except:
+                guess += 0
+    #print(guess)
     if guess > 0:
         if tweets.loc[tweets['text'] == i, 'stock_mov'].iloc[0] > 0:
             correct += 1
@@ -73,14 +91,20 @@ for i in tweets['text'][400:]:
         else:
             pass
 
+print(correct)
+print(wrong)
+
 print(100*correct/(correct+wrong))
 
+#words_weight['pence']
 
+#model = word2vec.load('text8.bin')
 
+# for i in words_weight:
+#     print(i, words_weight[i])
 
+#indexes, metrics = model.similar("shelter")
 
-
-
-
+#print(model.generate_response(indexes, metrics).tolist())
 
 
