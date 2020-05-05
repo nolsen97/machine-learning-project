083c993a2ed40a1173f6f7ca38a3c8fd87e4603e
Nate Olsen <nso4wg@virginia.edu>
2020-05-05 16:13:06 -0400

    Update treain_word2vec.py

--- treain_word2vec.py
+++ treain_word2vec.py
@@ -2,8 +2,8 @@
 
 import word2vec
 
-#word2vec.word2phrase('../project_extra/text8', '../project_extra/text8-phrases', verbose=True)
-word2vec.word2vec('./text8', './text8.bin', size=100, verbose=True)
+word2vec.word2phrase('./text8', './text8-phrases', verbose=True)
+word2vec.word2vec('./text8-phrases', './text8.bin', size=100, verbose=True)
 
 # model = word2vec.load('../project_extra/text8.bin')
 # indexes, metrics = model.similar("dog")
